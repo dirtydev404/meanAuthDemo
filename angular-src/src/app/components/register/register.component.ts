25ac72a486de06781406ee834d95702569251bed
Thad <thadlounds@gmail.com>
2018-12-08 08:56:02 -0700

    getting form validatoin and registration working, 1/2 through

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  fName: String;
+  lName: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(123);
+    const user = {
+      fname: this.fName,
+      lname: this.lName,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+    // Required Feilds
+    if (!this.validateService.validateRegister(user.email)) {
+      console.log('Please fill out everything!');
+      return false;
+    }
+
+    // Validate Email
+    if (!this.validateService.validateEmail(user.email)) {
+      console.log('Please enter a valid Email');
+      return false;
+    }
+  }
 }
