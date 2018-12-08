c34c1902af5ac21dfd3079331e823bb83e222234
Thad <thadlounds@gmail.com>
2018-12-08 11:04:16 -0700

    validate servie

--- angular-src/src/app/services/validate.service.ts
+++ angular-src/src/app/services/validate.service.ts
@@ -8,15 +8,17 @@ export class ValidateService {
   constructor() { }
 
   validateRegister(user) {
-    if (user.fName === undefined || user.lName === undefined || user.email === undefined || user.username === undefined || user.password === undefined) {
+    if (user.fName === undefined || user.lName === undefined || user.email === undefined ||
+      user.username === undefined || user.password === undefined) {
       return false;
     } else {
       return true;
     }
   }
 
-  validateEmail(email){
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@
+  ((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
   }
 }
