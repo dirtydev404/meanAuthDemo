48c191c239aaa6cef211f2ae2dfc590b135fe611
Thad <thadlounds@gmail.com>
2018-12-07 19:39:37 -0700

    changes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-=======
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
->>>>>>> 853c03a087cdda08f25540e7f0f8f95f8535961c
 import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -23,14 +18,8 @@ const appRoutes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
-<<<<<<< HEAD
   {path: 'profile', component: ProfileComponent},
 ];
-=======
-  {path: 'profile', component: ProfileComponent}
-
-]
->>>>>>> 853c03a087cdda08f25540e7f0f8f95f8535961c
 
 @NgModule({
   declarations: [
@@ -45,15 +34,9 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-<<<<<<< HEAD
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes)
-=======
-    HttpModule,
-    RouterModule.forRoot(appRoutes)
-
->>>>>>> 853c03a087cdda08f25540e7f0f8f95f8535961c
   ],
   providers: [],
   bootstrap: [AppComponent]
